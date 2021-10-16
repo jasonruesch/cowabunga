d8336d08654901466f019241b2742f5bdc5db6fe
Jason Ruesch <jruesch@degreed.com>
2021-10-16 11:29:06 -0400

    chore: fix hosting proxy issue for api app

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,13 +6,26 @@ import * as functions from 'firebase-functions';
 import 'firebase-admin';
 import { AppModule } from './app/app.module';
 
+const API_PREFIX = '/api';
 const server = express();
+
+function correctProxyUrl(req, _, next) {
+  // Fix hosting rewriting issue
+  if (req.url.indexOf(API_PREFIX) === 0) {
+    req.url = req.url.replace(API_PREFIX, '');
+    if (!req.url) {
+      req.url = '/';
+    }
+  }
+  next();
+}
 
 async function createNestServer(expressInstance) {
   const app = await NestFactory.create(
     AppModule,
     new ExpressAdapter(expressInstance)
   );
+  app.use(correctProxyUrl);
   return app.init();
 }
 
